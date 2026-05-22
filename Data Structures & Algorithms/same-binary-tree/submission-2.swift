23a56ceb9a296bf5677f1a548b88bbf793501935
Barry0327 <47265371+Barry0327@users.noreply.github.com>
2026-05-22 12:23:11 +0800

    Add: same-binary-tree - submission-2

---
+++ Data Structures & Algorithms/same-binary-tree/submission-2.swift
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     var val: Int
+ *     var left: TreeNode?
+ *     var right: TreeNode?
+ *     init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
+        if p == nil, q == nil {
+            return true
+        }
+
+        if let p, let q, p.val == q.val {
+            let left = isSameTree(p.left, q.left)
+            let right = isSameTree(p.right, q.right)
+            return left && right
+        } else {
+            return false
+        }
+    }
+}

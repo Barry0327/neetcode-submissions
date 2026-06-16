c4c9c6c76668277a0ed7489930b6911603d4a4c2
Barry0327 <47265371+Barry0327@users.noreply.github.com>
2026-06-16 09:07:00 +0800

    Add: lowest-common-ancestor-in-binary-search-tree - submission-0

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-0.swift
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     var val: Int
+ *     var left: TreeNode?
+ *     var right: TreeNode?
+ *     init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func lowestCommonAncestor(_ root: TreeNode?, _ p: TreeNode?, _ q: TreeNode?) -> TreeNode? {
+        var current = root
+
+        while current != nil {
+            let value = current!.val
+            if p!.val < value, q!.val < value {
+                current = current?.left
+            } else if p!.val > value, q!.val > value {
+                current = current?.right
+            } else {
+                return current
+            }
+        }
+
+        return current
+    }
+}

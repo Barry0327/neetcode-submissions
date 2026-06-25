a2f89bab2ed04dda7e95a2b0b88a110dba3823f3
Barry0327 <47265371+Barry0327@users.noreply.github.com>
2026-06-25 12:18:04 +0800

    Add: kth-largest-element-in-an-array - submission-6

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-6.swift
@@ -0,0 +1,35 @@
+class Solution {
+    func findKthLargest(_ nums: [Int], _ k: Int) -> Int {
+        var nums = nums
+        let targetIndex = nums.count - k
+
+        func partition(_ l: Int, _ r: Int) -> Int {
+            let pivot = r
+            var i = l
+            for j in l..<r {
+                if nums[j] <= nums[pivot] {
+                    nums.swapAt(i, j)
+                    i += 1
+                }
+            }
+            nums.swapAt(pivot, i)
+            return i
+        }
+
+        var l = 0, r = nums.count - 1
+        var pivot = nums.count
+        while pivot != targetIndex {
+            pivot = partition(l, r)
+            if pivot < targetIndex {
+                l = pivot + 1
+            } else if pivot > targetIndex {
+                r = pivot - 1
+            } else {
+                return nums[pivot]
+            }
+        }
+
+        return nums[pivot]
+        
+    }
+}

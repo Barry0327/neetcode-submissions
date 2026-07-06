776e9fe7198c07be044d1f9d5129c7936d109ee0
Barry0327 <47265371+Barry0327@users.noreply.github.com>
2026-07-06 11:31:44 +0800

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.swift
@@ -0,0 +1,26 @@
+class Solution {
+    func combinationSum(_ nums: [Int], _ target: Int) -> [[Int]] {
+        var result = [[Int]]()
+        var current = [Int]()
+
+        func backtrack(_ start: Int, _ remaining: Int) {
+            if remaining == 0 {
+                result.append(current)
+                return
+            }
+
+            if remaining < 0 {
+                return
+            }
+
+            for i in start..<nums.count {
+                current.append(nums[i])
+                backtrack(i, remaining - nums[i])
+                current.removeLast()
+            }
+        }
+
+        backtrack(0, target)
+        return result
+    }
+}
